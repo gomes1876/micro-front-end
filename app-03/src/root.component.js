fe3c9b96ba2c8ba10f4b5744bd6270bbedca13e6
Gabriel Gomes <gabriel.oliveira@codenapp.com>
2023-08-08 21:48:47 -0300

    parte fixa

---
+++ app-03/src/root.component.js
@@ -0,0 +1,5 @@
+import FixedView from "./screen/FixedView";
+
+export default function Root(props) {
+  return <section><FixedView /></section>;
+}
---
+++ app-03/src/screen/FixedView/index.js
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react"
+
+export default function () {
+
+    const [valApp01, setValApp01] = useState("sem valor");
+    const [valApp02, setValApp02] = useState("sem valor");
+    const [valShared, setValShared] = useState("sem valor");
+
+    function getLocalStorage() {
+        const infoApp01 = localStorage.getItem("app-01");
+        const infoApp02 = localStorage.getItem("app-02");
+        const rootStore = localStorage.getItem("root-store");
+
+        setValApp01(infoApp01)
+        setValApp02(infoApp02)
+        setValShared(rootStore)
+
+    }
+
+    useEffect(() => {
+        getLocalStorage()
+    }, [])
+
+    return (
+        <>
+            <h3>
+                Os dados aqui presentes só são atualizados se recarregar a aplicação, ou ao ela iniciar
+            </h3>
+            <p>{valApp01}</p>
+            <p>{valApp02}</p>
+            <p>{valShared}</p>
+        </>
+    )
+
+}
